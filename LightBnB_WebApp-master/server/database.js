866b49146ac5b78e0993a7f42620c7e8d33284a7
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-26 23:15:49 +0000

    Added search functionality, including min max price, rating, and city.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -96,7 +96,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT properties.*, reservations.*
+    FROM properties
+    JOIN reservations ON reservations.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    AND end_date < now():: date
+    GROUP BY properties.id, reservations.id
+    ORDER BY start_date
+    LIMIT $2
+    `, [guest_id, limit])
+    .then(res => res.rows)
+    .catch(err => null)
+  
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -110,11 +123,72 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = function (options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-    .then(res => res.rows);
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3.0
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  //3.1 (owner_id) was I even supposed to do this one? there isn't a slot for that
+  // on my page?????
+
+  // 3.2 (pricing min)
+  if (options.minimum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryParams.push(parseInt(options.minimum_price_per_night));
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    } else {
+    queryParams.push(parseInt(options.minimum_price_per_night));
+    queryString += `WHERE cost_per_night >= $${queryParams.length} `;
+    }
+  }
+
+  //3.3 (pricing max) 
+  if (options.maximum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryParams.push(parseInt(options.maximum_price_per_night));
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    } else {
+    queryParams.push(parseInt(options.maximum_price_per_night));
+    queryString += `WHERE cost_per_night <= $${queryParams.length} `;
+    }
+  }
+
+  // 3.4 (minimum rating)
+  if (options.minimum_rating) {
+    if (queryParams.length > 0) {
+      queryParams.push(parseInt(options.minimum_rating));
+      queryString += `AND rating >= $${queryParams.length} `;
+    } else {
+      queryParams.push(parseInt(options.minimum_rating));
+      queryString += `WHERE rating >= $${queryParams.length} `;
+    }
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(error => console.log(error));
 }
 exports.getAllProperties = getAllProperties;
 
